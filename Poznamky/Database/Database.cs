d399447e2ace21bdb861d4093a64a3e0f54e5cbd
Jiří Přemyslovský <j.premyslovsky@gmail.com>
2020-10-29 03:21:05 +0100

    databáze

---
+++ Poznamky/Database/Database.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Poznamky.Model;
+using SQLite;
+
+namespace Poznamky.Database
+{
+    class Database
+    {
+        private SQLiteConnection db;
+
+        public Database()
+        {
+            db = new SQLiteConnection(
+                Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), 
+                    "poznamky.db3"
+                )
+            );
+        }
+
+        public TableQuery<Note> getAll()
+        {
+            return db.Table<Note>();
+        }
+
+        public List<Note> getById(int id)
+        {
+            return db.Query<Note>("SELECT * FROM poznamky WHERE id = ?", id);
+        }
+
+        public int add(string name, string text)
+        {
+            Note note = new Note();
+
+            note.Name = name;
+            note.Text = text;
+            note.Creation_Date = new DateTime();
+
+            return db.Insert(note);
+        }
+
+        public int edit(int id, string name, string text)
+        {
+            Note note = new Note();
+
+            note.ID = id;
+            note.Name = name;
+            note.Text = text;
+            note.Edit_Date = new DateTime();
+
+            return db.Update(note);
+        }
+
+        public int delete(int id)
+        {
+            Note note = new Note();
+
+            note.ID = id;
+
+            return db.Delete(note);
+        }
+    }
+}
---
+++ Poznamky/Model/Note.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Poznamky.Model
+{
+    [Table("poznamky")]
+    class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime Creation_Date { get; set; }
+
+        public DateTime Edit_Date { get; set; }
+    }
+}
